96ab5568cdae2298a9681c666df871e65a8b45ca
Eugen-www <65528143+Eugen-www@users.noreply.github.com>
2020-12-11 17:46:44 +0300

    end the project

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-<<<<<<< HEAD
 import App from "./app/app";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
-=======
-const el = (
-  <div>
-    <h1>My Todo list</h1>
-    <input placeholder="search"/>
-    <ul>
-      <li>Learn React</li>
-      <li>Build awesome app</li>
-    </ul>
-  </div>
-);
-
-ReactDOM.render(el, document.getElementById("root"));
->>>>>>> 1cd92ab21b141eb596934df4581252ca3b39b9cf
+ReactDOM.render(<App />, document.getElementById("root"));
